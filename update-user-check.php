176cf2539e7ae411afa8a7260c567b7ffa867164
ljiljamatic <ljiljamatic99@gmail.com>
2021-03-25 15:54:15 +0100

    Add navbar

--- update-user-check.php
+++ update-user-check.php
@@ -16,6 +16,37 @@ session_start();
 </head>
 
 <body>  
+<?php
+$username = $_SESSION['username'];
+$qry = "SELECT * FROM users WHERE username='$username' ";
+$res = mysqli_query($conn, $qry);
+
+while($row = mysqli_fetch_assoc($res)){
+    $name = $row['name'];
+    $surname = $row['surname'];
+    $profilepic = $row['profilepic'];
+}
+?>
+
+<div class="row">
+    <div class="col-md-12 column">
+        <a href="home-customer.php">Početna stranica</a>
+        <div class="right-div">
+            <h5>You are logged in as <?php echo $_SESSION['username']; ?>
+            <a class="nav-link dropdown-toggle right-a" href="#" id="navbardrop" data-toggle="dropdown">
+                <img src="profile-pics/<?php echo $profilepic; ?>" width="40px" height="40px">
+            </a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="profile.php">View Profile</a>
+                <a class="dropdown-item" href="edit-profile.php">Edit Profile</a>
+                <a class="dropdown-item" href="logout.php">Logout</a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+
 
 <div class="add" id="add">
 <form action="" method="POST" enctype="multipart/form-data">
