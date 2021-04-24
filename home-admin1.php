8de0b64fc9a677f3c2ddd294b0bb6ce456f7ed72
ljiljamatic <ljiljamatic99@gmail.com>
2021-04-24 13:10:41 +0200

    Update

--- home-admin1.php
+++ home-admin1.php
@@ -23,8 +23,8 @@ if(isset($_SESSION['role'])){
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="home-admin1.css">
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="home-customer.css">
 </head>
 <body>
 
